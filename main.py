a990bc8a37da928c85b2cbf05c21af124d160237
Wasi-Muzammil <wasimuzammil2005@gmail.com>
2026-07-30 21:11:19 +0500

    Deployment State

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import router
+from routes import router
 
 app = FastAPI(
     title="Home Automation - Device Gateway",
